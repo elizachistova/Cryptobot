27ca267fbd6e7220e46ee84a7a26e8f88f5cc0d0
Eliza <eliza.chistova@gmail.com>
2025-01-16 10:23:50 +0100

    Other: changing airflow configuration

--- project_cryptobot/airflow/dags/cryptobot_dag.py
+++ project_cryptobot/airflow/dags/cryptobot_dag.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
-#Defining the DAG
+
 with DAG(
     'cryptobot_workflow',
     default_args={
@@ -11,7 +11,7 @@ with DAG(
     },
     description='A workflow for cryptobot',
     # Every 4 hours
-    schedule_interval='*/4 * * * *',
+    schedule_interval='0 */4 * * *',
     start_date=datetime(2025, 1, 1),
     catchup=False
 ) as dag:
@@ -19,32 +19,32 @@ with DAG(
     # Task 1: Data Collection
     fetch_data = BashOperator(
         task_id='fetch_data',
-        bash_command='',
+        bash_command="python ../../src/extract.py",
     )
 
     # Task 2: Preprocess Data
     preprocess_data = BashOperator(
         task_id='preprocess_data',
-        bash_command='',
+        bash_command='python ../../src/Data_processor.py',
     )
 
     # Task 3: Model Prediction
     model_prediction = BashOperator(
         task_id='model_prediction',
-        bash_command='',
+        bash_command='python ../../src/predictions.py',
     )
 
     # Task 4: Database Update
     db_update = BashOperator(
         task_id='db_update',
-        bash_command='',
+        bash_command='python ../../src/fastapi-mongo.py',
     )
 
     # Task 5: Dashboard Update
-    dashboard_update = BashOperator(
-        task_id='dashboard_update',
-        bash_command='',
-    )
+    #dashboard_update = BashOperator(
+     #   task_id='dashboard_update',
+      #  bash_command='',
+    #)
 
     # Define task dependencies
-    fetch_data >> preprocess_data >> model_prediction >> db_update >> dashboard_update
+    fetch_data >> preprocess_data >> model_prediction >> db_update 
